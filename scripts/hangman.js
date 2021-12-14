8b999679f9a6e29403bcb7fa0beae8abb2fd50d5
nathanielpells <93220158+nathanielpells@users.noreply.github.com>
2021-12-14 20:48:18 +0000

    Making hangman show and word come randomly

--- scripts/hangman.js
+++ scripts/hangman.js
@@ -1,12 +1,23 @@
-console.log('test');
-
-let word = 'pells';
+// console.log('test');
+function randomIntFromInterval(min, max) { // min and max included
+    return Math.floor(Math.random() * (max - min + 1) + min)
+    }
+    let maxNumber = words.length;
+    const rndInt = randomIntFromInterval(0, maxNumber);
+    
+    console.log(maxNumber);
+    console.log(rndInt);
+    
+    let word = words[rndInt];
+    console.log(word);
 
 
 
 let allLetters = document.querySelectorAll('.alphabetContainer .letter');
 let guessContainer = document.querySelector('.guessContainer');
+let wrongGuessContainer = document.querySelector('.wrongGuessContainer');
 // console.log(allLetters);
+console.log(wrongGuessContainer);
 
 let guessLetterHTML = '<div class="guessLetter"></div>';
 for(counter=0; counter < word.length; counter++){
@@ -14,7 +25,7 @@ for(counter=0; counter < word.length; counter++){
 }
 
 let guessLetters = document.querySelectorAll('.guessLetter');
-
+let lifeCounter=0;
 allLetters.forEach(function (letter){
     // console.log(letter)
     letter.addEventListener('click',function(event){
@@ -23,8 +34,8 @@ allLetters.forEach(function (letter){
             let currentLetter = event.target.innerHTML.toLowerCase(); 
             currentLetterHTML.classList.add('clicked');
             let answers =[...word.matchAll(currentLetter)];
-            // console.log(answers);
-            if(answers){
+            console.log(answers);
+            if(answers.length){
                 answers.forEach(function(answer){
                     // console.log(answer.index);
                     for(counter=0; counter < guessLetters.length; counter++){
@@ -33,11 +44,42 @@ allLetters.forEach(function (letter){
                             guessLetters[answer.index].innerHTML = currentLetter.toUpperCase();
                         }
                     }
+                    checkWin();
                 })
             } else {
                 //show the next part of the hangman.
+                let wrongGuessLetterHTML = '<div class="wrongGuessLetter">'+currentLetter.toUpperCase()+'</div>';
+                wrongGuessContainer.insertAdjacentHTML( 'beforeend', wrongGuessLetterHTML);
+                lifeCounter++;
+                // console.log(lifeCounter);
+                let currentLineSelector = '.life'+lifeCounter;
+                let currentLifeElement = document.querySelector(currentLineSelector);
+                currentLifeElement.classList.add('active');
+                if(lifeCounter == 11){
+                    endGame();
+                }
             }
         } 
     }); 
 });
 
+function checkWin(){
+    blnGameWon = true;
+    for(counter=0; counter < guessLetters.length; counter++){
+        let innerHTML = guessLetters[counter].innerHTML;
+        // console.log(innerHTML);
+        if(!innerHTML){
+            blnGameWon = false;
+        }
+    }
+    if(blnGameWon){
+        endGame();
+    }
+}
+
+function endGame(){
+    allLetters.forEach(function (letter){
+        console.log(letter);
+        letter.classList.add('clicked');
+    });
+}
